a46e8193889db9116f1f149f75396a7c83b3b5f4
Chase Nickel <nickel5@outlook.com>
2024-05-28 01:25:53 -0500

    Rename build.zig output executable to brainf

--- build.zig
+++ build.zig
@@ -4,7 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
     const exe = b.addExecutable(.{
-        .name = "brainf-compiler",
+        .name = "brainf",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
